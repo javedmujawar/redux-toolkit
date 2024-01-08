2752fd8ace34d7d4c6f342e90f73e89ff6548ec4
stringcoretech <stringcoretech@gmail.com>
2024-01-08 18:08:45 +0530

    redux toolkit store, action and dispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { increment,decrement } from './redux/counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {count} = useSelector(state=> state.counter) 
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>The Count Is: {count}</h1>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
-      <button onClick={()=>setCount(count-1)}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
